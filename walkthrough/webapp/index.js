813799fb76b57e0c9a8550ffd86ecf85ed0b292d
Stephanie Tan Jia Yi <steffijyt@hotmail.com>
2021-01-25 22:37:32 +0800

    Step 2: Bootstrap

---
+++ walkthrough/webapp/index.js
@@ -0,0 +1,10 @@
+// index.js script that will contain the application logic for this tutorial step
+// we do this to avoid having executable code directly in the HTML file for security reasons
+// this script will be called by the index.html, we defined it there as a module in a declarative way
+sap.ui.define([
+
+], function () {
+    "use strict";
+
+    alert("UI5 is ready");
+});
