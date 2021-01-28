1989222c40756c4dab245035f9e239f0413a6874
Stephanie Tan Jia Yi <steffijyt@hotmail.com>
2021-01-28 22:49:15 +0800

    Step 16: Dialogs and Fragments

--- walkthrough/webapp/controller/HelloPanel.controller.js
+++ walkthrough/webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,9 @@
 // to have a reusable asset, the method onShowHello is also moved from the app controller to the HelloPanel controller
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
     "use strict";
     return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
         // onShowHello event handler function
@@ -20,6 +21,36 @@ sap.ui.define([
             var sMsg = oBundle.getText("helloMsg", [sRecipient]);
             // show message
             MessageToast.show(sMsg);
+        },
+        // if the dialog in the fragment does not exist yet, the fragment is instantiated by calling the Fragment.load API with the following arguments:
+        // 1. the ID of the HelloPanel view
+        // this parameter is used to prefix the IDs inside our fragment
+        // there, we have defined the ID helloDialog for the Dialog control, and we can access the dialog via the view by calling oView.byId("helloDialog")
+        // this makes sure that even if you instantiate the same fragment in other views in the same way, each dialog will have its unique ID that is concatenated from the view ID and the dialog ID
+        // using unique IDs is important, because duplicate IDs lead to errors in the framework
+        // 2. the name of the fragment
+        // we add the dialog as "dependent" on the view to be connected to the lifecycle of the view’s model
+        // a convenient side-effect is that the dialog will automatically be destroyed when the view is destroyed
+        // otherwise, we would have to destroy the dialog manually to free its resources
+        // always use the addDependent method to connect the dialog to the lifecycle management and data binding of the view, even though it is not added to its UI tree
+        // private functions and variables should always start with an underscore
+        onOpenDialog: function () {
+            var oView = this.getView();
+
+            // create dialog lazily
+            if (!this.pDialog) {
+                this.pDialog = Fragment.load({
+                    id: oView.getId(),
+                    name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                }).then(function (oDialog) {
+                    // connect dialog to the root view of this component (models, lifecycle)
+                    oView.addDependent(oDialog);
+                    return oDialog;
+                });
+            }
+            this.pDialog.then(function (oDialog) {
+                oDialog.open();
+            });
         }
     });
 });
